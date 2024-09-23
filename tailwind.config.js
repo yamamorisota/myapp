72ce37baad44e1caaa985e2452508bea92758e2f
yamamorisota <sota787878@gmail.com>
2024-09-23 16:24:57 +0900

    tailwind:cdnを利用したヘッダー

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/*.{html, js}',
-    './public/css/styles.css',
+    './src/input.css',
     './public/output.css',
   ],
   theme: {
